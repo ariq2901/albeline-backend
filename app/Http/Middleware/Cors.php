a874c6480fa29a68d4da6220cefa34921d9fec5a
Ariq2901 <ariqkage@gmail.com>
2021-03-20 18:18:59 +0700

    update cors middleware issues

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,8 +17,8 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         // header('Access-Control-Allow-Origin:  http://localhost:3000');
-        header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
-        header('Access-Control-Allow-Methods:  GET, POST, PUT, DELETE');
+        // header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
+        // header('Access-Control-Allow-Methods:  GET, POST, PUT, DELETE');
         return $next($request)->header('Access-Control-Allow-Origin', '*')
                                 ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     }
