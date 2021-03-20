1d28eba0a4240fab203d25add00f9c1fe45b9528
Ariq2901 <ariqkage@gmail.com>
2021-03-20 18:15:42 +0700

    update cors middleware issue

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,6 +19,7 @@ class Cors
         // header('Access-Control-Allow-Origin:  http://localhost:3000');
         header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
         header('Access-Control-Allow-Methods:  GET, POST, PUT, DELETE');
-        return $next($request);
+        return $next($request)->header('Access-Control-Allow-Origin', '*')
+                                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -47,9 +47,11 @@ Route::post('/password/email', [ForgotPasswordAPIController::class, 'sendResetLi
 Route::get('/store/{id}', 'App\Http\Controllers\StoreController@show');
 
 //^ Raja Ongkir
-Route::get('/cities', 'App\Http\Controllers\RajaOngkirController@getKota');
-Route::get('/provinces', 'App\Http\Controllers\RajaOngkirController@getProvinces');
-Route::post('/cost', 'App\Http\Controllers\RajaOngkirController@getCost');
+Route::middleware('cors')->group(function() {
+    Route::get('/cities', 'App\Http\Controllers\RajaOngkirController@getKota');
+    Route::get('/provinces', 'App\Http\Controllers\RajaOngkirController@getProvinces');
+    Route::post('/cost', 'App\Http\Controllers\RajaOngkirController@getCost');
+});
 
 //^ Auth
 Route::group(['middleware' => 'auth:api', 'CheckVerified'], function() {
